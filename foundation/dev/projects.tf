409f1fd8e27a265986c49538fca0f712507be451
wanderson <wanderson.aju@outlook.com>
2025-02-25 13:50:33 -0300

    update

--- foundation/dev/projects.tf
+++ foundation/dev/projects.tf
@@ -3,8 +3,8 @@ module "projects" {
   organization_id = var.organization_id
   billing_account = var.billing_account
   projects = [
-    { name = "wfs-network-dev", folder = "Network" },
-    { name = "wfs-lab-dev", folder = "Desenvolvimento" }
+    { name = "wfs-network-dev", folder = 1098604734150 },
+    { name = "wfs-lab-dev", folder = 984690173317 }
   ]
   depends_on = [module.subfolders]
 }
