5b111ec36faf8b58b133f50feb850f7deb7b28d2
wandersonaju <120660042+wandersonaju@users.noreply.github.com>
2025-11-09 12:43:10 -0300

    fix caldo de cana domain

--- dns/domain/domain_parceiros.tf
+++ dns/domain/domain_parceiros.tf
@@ -16,7 +16,7 @@ module "caldo-de-cana" {
   dns_name  = "caldodecana.com.br."
   dns_records = {
 
-    "blog" = { name = "blog.caldodecana.com.br.", records = ["203.0.113.12"], ttl = 300, type = "A" }
+    "blog" = { name = "blog.caldodecana.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
     "shop" = { name = "shop.caldodecana.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
 
   }
