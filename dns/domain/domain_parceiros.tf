4325dba481654491f98a9ede060c528a4658484c
wandersonaju <120660042+wandersonaju@users.noreply.github.com>
2025-11-10 10:27:01 -0300

    Refactor DNS modules for new domain names
    Updated DNS records and zone names for multiple modules to reflect new domain structure.

--- dns/domain/domain_parceiros.tf
+++ dns/domain/domain_parceiros.tf
@@ -1,47 +1,55 @@
 module "pastel" {
-  source    = "../module"
-  zone_name = "coxinha-pastel-com-br"
-  dns_name  = "coxinha.pastel.com.br."
+  source      = "../module"
+  zone_name   = "pastel-barraca-com-br"
+  dns_name    = "pastel.barraca.com.br."
+  main_domain = module.main_domain.zone_name
   dns_records = {
 
-    "blog" = { name = "blog.coxinha.pastel.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
-    "shop" = { name = "shop.coxinha.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "blog" = { name = "pastel.barraca.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
+    "shop" = { name = "www.pastel.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
 
   }
+  depends_on = [ module.main_domain ]
 }
 
 module "caldo-de-cana" {
   source    = "../module"
-  zone_name = "caldo-pastel-com-br"
-  dns_name  = "caldo.pastel.com.br."
+  zone_name = "caldo-barraca-com-br"
+  dns_name  = "caldo.barraca.com.br."
+  main_domain = module.main_domain.zone_name
   dns_records = {
 
-    "blog" = { name = "blog.caldo.pastel.com.br.", records = ["203.0.113.12"], ttl = 300, type = "A" }
-    "shop" = { name = "shop.caldo.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "blog" = { name = "caldo.barraca.com.br.", records = ["203.0.113.12"], ttl = 300, type = "A" }
+    "shop" = { name = "shop.caldo.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
 
   }
+  depends_on = [ module.main_domain ]
 }
 
 module "camarao" {
   source    = "../module"
-  zone_name = "camarao-pastel-com-br"
-  dns_name  = "camarao.pastel.com.br."
+  zone_name = "camarao-barraca-com-br"
+  dns_name  = "camarao.barraca.com.br."
+  main_domain = module.main_domain.zone_name
   dns_records = {
 
-    "blog" = { name = "blog.camarao.pastel.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
-    "shop" = { name = "shop.camarao.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
-    "www"  = { name = "www.camarao.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "blog" = { name = "blog.camarao.barraca.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
+    "shop" = { name = "shop.camarao.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "www"  = { name = "www.camarao.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
   }
+  depends_on = [ module.main_domain ]
 }
 
 module "cocacola" {
   source    = "../module"
-  zone_name = "cocacola-pastel-com-br"
-  dns_name  = "cocacola.pastel.com.br."
+  zone_name = "cocacola-barraca-com-br"
+  dns_name  = "cocacola.barraca.com.br."
+  main_domain = module.main_domain.zone_name
   dns_records = {
 
-    "blog" = { name = "blog.cocacola.pastel.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
-    "shop" = { name = "shop.cocacola.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
-    "www"  = { name = "www.cocacola.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "blog" = { name = "blog.cocacola.barraca.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
+    "shop" = { name = "shop.cocacola.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "www"  = { name = "www.cocacola.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
   }
+  depends_on = [ module.main_domain ]
 }
