152f15535f2f585fcea8cf0cfda9031807029641
wandersonaju <120660042+wandersonaju@users.noreply.github.com>
2025-11-10 10:27:19 -0300

    Replace dominio_principal module with main_domain

--- dns/domain/domain_principal.tf
+++ dns/domain/domain_principal.tf
@@ -1,13 +1,13 @@
-module "dominio_principal" {
+module "main_domain" {
   source    = "../module"
-  zone_name = "pastel-com-br"
-  dns_name  = "pastel.com.br."
+  zone_name = "barraca-com-br"
+  dns_name  = "barraca.com.br."
   dns_records = {
 
-    "www"   = { name = "www.pastel.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
-    "api"   = { name = "api.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
-    "caixa" = { name = "caixa.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
-    "bb"    = { name = "bb.pastel.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "www"   = { name = "www.barraca.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
+    "api"   = { name = "api.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "caixa" = { name = "caixa.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
+    "bb"    = { name = "bb.barraca.com.br.", records = ["203.0.113.11"], ttl = 300, type = "A" }
 
   }
 }
