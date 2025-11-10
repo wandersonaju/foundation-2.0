0a2fdc6ee1d471e50dd2e9f513089db6a9ca3b06
wandersonaju <120660042+wandersonaju@users.noreply.github.com>
2025-11-10 10:53:57 -0300

    add coxinha

--- dns/domain/domain_parceiros.tf
+++ dns/domain/domain_parceiros.tf
@@ -53,3 +53,23 @@ module "cocacola" {
   }
   depends_on = [ module.main_domain ]
 }
+
+module "coxinha" {
+  source      = "../module"
+  zone_name   = "coxinha-barraca-com-br"
+  dns_name    = "coxinha.barraca.com.br."
+  main_domain = module.main_domain.zone_name
+  dns_records = {
+
+    "blog"      = { name = "coxinha.barraca.com.br.", records = ["203.0.113.10"], ttl = 300, type = "A" }
+    "txt_test"  = { name = "coxinha.barraca.com.br.", records = [
+    "\"random-verification=4f1c9e8d3a2b\"",
+    "\"google-site-verification=7e9f8c1d2b34\"",
+    "\"env=test-lab\"",
+    "\"owner=infra-team\""
+  ], 
+  ttl = 300, type = "TXT" 
+  }
+}
+  depends_on = [ module.main_domain ]
+}
